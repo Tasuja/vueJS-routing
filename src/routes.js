6715348817d7213848e61ad23cb3ebc0cb8c81ee
Tasuja <sujata@vayu.com.au>
2018-07-16 16:42:44 +0545

    Created links with named routes

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ import UserStart from './components/user/UserStart.vue';
 
 
 export const routes = [
-  { path: '', component: Home},
+  { path: '', component: Home, name: 'home'},
   { path: '/user', component: User, children: [
   	{path: '', component: UserStart},
   	{path: ':id', component: UserDetail},
-  	{path: ':id/edit', component: UserEdit}
+  	{path: ':id/edit', component: UserEdit, name:'userEdit'}
   ]}
 ];
